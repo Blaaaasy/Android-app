a164a3c06ff537c1491eb997ea5da34b7ccc5310
layla615 <531965539@qq.com>
2022-09-05 18:22:28 +0800

    add addFriendButtom

--- app/src/main/java/com/example/myapplication1/login2Activity.java
+++ app/src/main/java/com/example/myapplication1/login2Activity.java
@@ -2,56 +2,61 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class login2Activity extends AppCompatActivity {
-//定义一个类
-public class User {
-    private String UserName;
-    private String UserDetails;
-    private int photo;
+////定义一个类
+//public class User {
+//    private String UserName;
+//    private String UserDetails;
+//    private int photo;
+//
+//    public User() {
+//    }
+//
+//    public User(String UserName, String UserDetails, int photo) {
+//        this.UserName = UserName;
+//        this.UserDetails = UserDetails;
+//        this.photo = photo;
+//    }
+//
+//    public String getName() {
+//        return UserName;
+//    }
+//
+//    public String getDetails() {
+//        return UserDetails;
+//    }
+//
+//    public int getPhoto() {
+//        return photo;
+//    }
+//
+//    public void setUserName(String UserName) {
+//        this.UserName = UserName;
+//    }
+//
+//    public void setUserDetails(String UserDetails) {
+//        this.UserDetails = UserDetails;
+//    }
+//
+//    public void setPhoto(int photo) {
+//        this.photo = photo;
+//    }
+//}
 
-    public User() {
-    }
-
-    public User(String UserName, String UserDetails, int photo) {
-        this.UserName = UserName;
-        this.UserDetails = UserDetails;
-        this.photo = photo;
-    }
-
-    public String getName() {
-        return UserName;
-    }
-
-    public String getDetails() {
-        return UserDetails;
-    }
-
-    public int getPhoto() {
-        return photo;
-    }
-
-    public void setUserName(String UserName) {
-        this.UserName = UserName;
-    }
-
-    public void setUserDetails(String UserDetails) {
-        this.UserDetails = UserDetails;
-    }
-
-    public void setPhoto(int photo) {
-        this.photo = photo;
-    }
-}
-
-
+private Button addFriends;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        addFriends=(Button)findViewById(R.id.addFriend);
 // 获取界面组件
         ListView listView = (ListView) findViewById(R.id.list_view);
 
@@ -64,6 +69,14 @@ public class User {
 
         // 为ListView设置Adapter
         listView.setAdapter(adapter);
+
+        addFriends.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(), "添加成功", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
