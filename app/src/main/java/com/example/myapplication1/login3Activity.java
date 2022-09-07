27e1189ac99484d7f827b84064bf2b87968d326d
Blasy <Blaaaasy@users.noreply.github.com>
2022-09-07 19:07:44 +0800

    Fix bug

--- app/src/main/java/com/example/myapplication1/login3Activity.java
+++ app/src/main/java/com/example/myapplication1/login3Activity.java
@@ -22,10 +22,10 @@ public class login3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_login3);
 
 
-        Intent reintent = getIntent();
-        String username = reintent.getStringExtra("user");
-        String savatar = reintent.getStringExtra("avatar");
-        int avatar = Integer.parseInt(savatar);
+//        Intent reintent = getIntent();
+//        String username = reintent.getStringExtra("user");
+//        String savatar = reintent.getStringExtra("avatar");
+//        int avatar = Integer.parseInt(savatar);
 
         addFriends = (Button) findViewById(R.id.addFriends);
         friends = (Button) findViewById(R.id.friends);
@@ -33,8 +33,8 @@ public class login3Activity extends AppCompatActivity {
         user = (TextView)findViewById(R.id.username);
         imageView=(ImageView)findViewById(R.id.imageView);
 
-        imageView.setImageResource(avatar);
-        user.setText(username);
+//        imageView.setImageResource(avatar);
+//        user.setText(username);
 
         logout = (Button) findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener()
